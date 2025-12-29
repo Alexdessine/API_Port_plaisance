c70e009eca9e26b9583ad98813f417709ca19592
Alexdessine <alexandre@bourlier.email>
2025-12-29 12:30:31 +0100

    Ajout MongoDB + test de connexion validé

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/**
+ * LE app.js reste le plus simple possible.
+ * On initialise ici la connexion à MongoDB.
+ * On utilise le package CORS installé précédemment en acceptant toutes les origines et en exposant le header "Authorization"
+ * (pour récupérer lr token d'authentification côté client)
+ * On déclare notre route principale avec pour url de base "/".
+ * On ajoute un retour en cas de requête sur une route inexistante (404)
+ */
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
 
+const indexRouter = require('./routes/index');
+const mongodb = require('./db/mongo');
+
+const path = require('path');
+
+mongodb.initClientDbConnection();
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(cors({
+    exposedHeaders: ['Authorization'],
+    origin: '*'
+}));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// const authView = require('./middlewares/auth-view');
+
+// app.use(authView.injectUserIfAny);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(function (req, res, next) {
+    res.status(404).json({ name: 'API', version: '1.0', status: 404, message: 'not_found' });
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  res.status(200).json({
+    name: process.env.APP_NAME,
+    version: '1.0',
+    status: 200,
+    message: 'Bienvenue sur l\'API !'
+  });
 });
 
 module.exports = router;
